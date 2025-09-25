6e88b8103cab5a64f280c7fb5435fb50d3f0282f
Yaroslav Korotenko <Kor1k13002@gmail.com>
2025-09-26 02:26:38 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 class Car:
-
-    def __init__(
-        self, comfort_class: int, clean_mark: int, brand: str
-    ) -> None:
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -28,23 +25,23 @@ class CarWashStation:
         weighted_rating = power_difference * self.average_rating
         car_score = car.comfort_class * weighted_rating
         price = car_score / self.distance_from_city_center
-        return round(price, 1)
+        return round(price, 2)  # округлення до 2 знаків
 
-    def wash_single_car(self, car: Car) -> None:
+    def wash_single_car(self, car: Car) -> float:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
+            return self.calculate_washing_price(car)
+        return 0
 
     def serve_cars(self, cars: list[Car]) -> float:
         total_price = 0
         for car in cars:
-            price = self.calculate_washing_price(car)
-            if price > 0:
-                self.wash_single_car(car)
-                total_price += price
-        return total_price
+            price = self.wash_single_car(car)  # виклик лише один раз
+            total_price += price
+        return round(total_price, 2)
 
     def rate_service(self, rate: int) -> None:
         total_rating = self.average_rating * self.count_of_ratings
         total_rating += rate
         self.count_of_ratings += 1
-        self.average_rating = round(total_rating / self.count_of_ratings, 1)
+        self.average_rating = round(total_rating / self.count_of_ratings, 2)
